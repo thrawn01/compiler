5c5198042c484b41961474a2c3f39ac78e7f3391
Derrick J. Wippler <thrawn01@gmail.com>
2011-08-24 22:20:42 +0000

    Discovered core syntax has not been flushed out, work in progress

--- syntax/function-types.js
+++ syntax/function-types.js
@@ -1,88 +1,63 @@
-// An experiment in function types and function signatures
+// -------------------------------------
+// Function types and Calling Examples
+// -------------------------------------
 
-
-// Very C++ ish
-Array<Any> filter( Array<Any> array, function<Boolean (Any)> condition ){
-                    
+// Types begin with a capital letter
+Array<Any> filter(Array<Any> array, Function<Boolean, (Any)> condition) {
+  // Code here
 }
 
-// More Scala ish
-Array[Any] filter( Array[Any] array, function[Boolean (Any)] condition ){
-                
-}
-
-// More like Scala / fortran
-filter( array: Array[Any], condition: (Any) => Boolean ) : Array[Any] {
-                
-}
-
-// Variation on Scala/C++
-Array[Any] filter( array: Array[Any], condition: (Any) => Boolean ) {
-                
-}
-
-// Scala/fortran but with the names after the type defs
-Array[Any] filter( Array[Any] array, (Any) => Boolean condition ) {
-                
-}
-
-// Anthony's Suggestion
-Array[Any] filter(Array[Any] array, Function[Boolean, Any] condition) {
-
-}
-
-// Should Allow 'typedef' to tidy up function signatures
-typedef Function[Boolean (Any)] FilterFunction
+// 'typedef' to tidy up function signatures
+typedef Function<Boolean (Any)> FilterFunction
 
 // With type defs, doesn't look so ugly, C++ style might work well
-Array[Any] filter(Array[Any] array, FilterFunction condition) {
-
+def(filter, Function<Array<Boolean>, (Array<Int> items)>, FilterFunction condition) {
+  // Code here
 }
 
 // ------------------------------
 // Calling Possiblities
 // ------------------------------
 
+// Explicit
+filter(Array<Int>(1,2), Function<Boolean (Any e)> { if eq(e,1) { return true } } )
+
 // For this example, the compiler can infer that e is of type Any
 // and the return type is Boolean; by looking at the signature 
 // the filter function requires
-filter([1,2], function(e) { if e == 1 { return true } })
-
-// Or? 
-filter([1,2], Function[e] { if e == 1 { return true } })
-// Should we distinguish the difference between calling and defining a function?
+filter(Array(1,2), Function<(e)> { if eq(e,1) { return true } } )
 
 
-// Using a named function
+// Defining a named function 
 def Boolean filterFunction( Any e ) {
     if e == 1 { 
         return true;
     }
 }
 
-// Here the compiler doesn't have to infer the arguments of the function
-filter([1,2], filterFunction );
-
-
-// 'def' could me a synonym for the following ( But I'm not really liking this much )
-filterFunction = Function[Boolean (Any e)] { 
+// Return type could be infered
+def filterFunction( Any e ) {
     if e == 1 { 
         return true;
     }
 }
 
-// ===================================
-// Crazy idea to create 2 meta-compiler functions 
-// to handle creating functions and signatures on 
-// behalf of the AST parser, this way anyone that doesn't
-// like our syntax for defining a method could create their own.
-// ===================================
+// Here the compiler doesn't have to infer the arguments of the function
+filter(Array<Float>(1,2), filterFunction );
 
-// first argument is the return type, next is first type, then type name, repeating...
-func_signature = create_function_signature(Boolean, Int, 'e')
-// Then we create the function defnition
-new_function = create_function( func_signature, { if e == 1 { return true } } )
 
-// now 'new_function' is a usable function
-value = new_function(1) // <-- value equals true
+// 'def' could me a synonym for the following ( But I'm not really liking this much )
+filterFunction = Function<Boolean (Any e)> { 
+    if e == 1 { 
+        return true;
+    }
+}
+
+// -------------------------------------
+// Symbol definition in the core language
+// -------------------------------------
+
+def(value,Float,1)
+def(sqrt, Function<Boolean (Float e)>, { e * e })
+
 
