bc4bff99e0088235ae4f1c614dd126217517b5cf
Derrick J. Wippler <thrawn01@gmail.com>
2012-08-16 03:25:49 +0000

    Some syntax parser ideas

--- syntax/parser-type-system.js
+++ syntax/parser-type-system.js
@@ -23,3 +23,29 @@ for (item in list; upped = item.upper()) {
     print item
     print upped
 }
+
+
+// some syntax ideas
+
+
+// first define the while function
+def while(conditional, block) {
+    // while implementation
+}
+
+// syntax keyword <bnf>
+//  ^      ^       ^
+//  |      |       ---- bnf regex
+//  |      --- bnf match keyword
+//  --- special form (core code keyword)
+
+// Now define the special syntax
+// instead of calling "while(1, {})
+// now you can call "while 1 {}"
+syntax while <some bnf parser syntax>
+    // everything under 'scope' shares the same scope
+   (scope 
+       // Places the 'while' symbol with the conditional and block 
+       // collected from the bnf parser syntax
+      (while, conditional, block))
+
